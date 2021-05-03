27e54fdf7bc06b2f104ced0cf992d789ed38f797
Chamika <edirisinghe.chamika.madushan@gmail.com>
2021-05-04 04:00:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/cmkapps/gradefive/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cmkapps.gradefive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
